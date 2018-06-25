3018e5944326ab8873f1642ab11577ce7098e856
renaud gaudin <rgaudin@gmail.com>
2018-06-25 14:41:26 +0000

    fixed exfat partition mounting on linux

--- pibox-installer/backend/mount.py
+++ pibox-installer/backend/mount.py
@@ -60,12 +60,8 @@ def get_start_offset(root_size, disk_size):
 
     nb_clusters_endofroot = root_size // sector_size
     root_end = roundup(nb_clusters_endofroot)
-    data_start = roundup(root_end + sector_size)
+    data_start = root_end + 1
     data_bytes = disk_size - root_size - end_margin
-    # data_clusters = data_bytes // sector_size
-    # data_end = data_start + data_clusters
-    # data_size = data_end - data_start
-    # data_size * sector_size
 
     return data_start * sector_size, data_bytes
 
